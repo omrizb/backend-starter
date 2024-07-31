022d720f855396a36dbb98668c3e5fdfdf2d6105
omribarouch <omri.barouch@safebreach.com>
2024-07-31 15:56:57 +0300

    Replace old backend with new mister backend

--- services/logger.service.js
+++ services/logger.service.js
@@ -1,47 +1,53 @@
 import fs from 'fs'
 
-export const loggerService = {
+const logsDir = './logs'
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir)
+}
+
+export const logger = {
     debug(...args) {
-        doLog('DEBUG', ...args)
+        if (process.env.NODE_NEV === 'production') return
+        _doLog('DEBUG', ...args)
     },
     info(...args) {
-        doLog('INFO', ...args)
+        _doLog('INFO', ...args)
     },
     warn(...args) {
-        doLog('WARN', ...args)
+        _doLog('WARN', ...args)
     },
     error(...args) {
-        doLog('ERROR', ...args)
+        _doLog('ERROR', ...args)
     }
-}
-
-const LOGS_DIR = './logs'
-const LOG_FILE = 'backend.log'
-
-if (!fs.existsSync(LOGS_DIR)) {
-    fs.mkdirSync(LOGS_DIR)
-}
-
-// Define the time format
-function doLog(level, ...args) {
-
-    const strs = args.map(arg =>
-        (typeof arg === 'string' || _isError(arg)) ? arg : JSON.stringify(arg)
-    )
-    var line = strs.join(' | ')
-    line = `${_getTime()} - ${level} - ${line}\n`
-
-    console.log(line)
-    fs.appendFile(`${LOGS_DIR}/${LOG_FILE}`, line, (err) => {
-        if (err) console.log('FATAL: cannot write to log file')
-    })
 }
 
 function _getTime() {
     let now = new Date()
-    return now.toLocaleString('he')
+    return now.toLocaleString('he') //define the time format
 }
 
 function _isError(e) {
     return e && e.stack && e.message
+}
+
+function _doLog(level, ...args) {
+
+    const strs = args.map(arg => {
+        if (typeof arg === 'string') {
+        } else if (_isError(arg)) {
+        } else if (arg instanceof Promise) {
+            arg = 'Promise'
+        } else {
+            console.log('STRINGIFY', arg)
+            arg = JSON.stringify(arg)
+        }
+        return arg
+    })
+
+    var line = strs.join(' | ')
+    line = `${_getTime()} - ${level} - ${line} \n`
+    console.log(line)
+    fs.appendFile('./logs/backend.log', line, (err) => {
+        if (err) console.log('FATAL: cannot write to log file')
+    })
 }
